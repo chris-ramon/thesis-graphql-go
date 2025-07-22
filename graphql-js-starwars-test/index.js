2673c993e5e18044fa565ef06aa567a821f29a8b
Chris Ramón <rchristian.ramon@gmail.com>
2025-07-21 19:57:08 -0500

    graphql-js-starwars-test: adds graphiql support

--- graphql-js-starwars-test/index.js
+++ graphql-js-starwars-test/index.js
@@ -19,6 +19,32 @@ const schema = new GraphQLSchema({
   }),
 });
 
-graphql(schema, `{ echo }`).then((result) => {
+graphql(
+  schema,
+  `
+    {
+      echo
+    }
+  `,
+).then((result) => {
   console.log(JSON.stringify(result.data, null, 4));
+});
+
+const express = require("express");
+const app = express();
+const port = 3000;
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", (req, res) => {
+  const query = "";
+  const response = JSON.stringify({});
+  const variables = JSON.stringify({});
+  const operationName = "";
+  res.render("index", { query, response, variables, operationName });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
